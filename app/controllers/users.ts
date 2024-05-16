53bedbe8b57b9bad6d7ed67ce798d71b457aa59b
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-16 23:11:13 +0700

    add: prisma query and refactor

--- app/controllers/users.ts
+++ app/controllers/users.ts
@@ -1,33 +1,40 @@
 import { Request, Response } from 'express';
-import db from '../configs/db.config';
-import respone from '../utils/respone';
-import bcryptjs from 'bcryptjs';
-import { ResultSetHeader } from 'mysql2';
+import { PrismaClient } from '@prisma/client';
 
-const getUsers = (req: Request, res: Response) => {
-    db.query("select * from tamu", (err, result) => {
-        if (err) {
-            respone(400, err, "error", res);
-        } else {
-            respone(200, result, "success", res);
+const prisma = new PrismaClient();
+
+const getUsers = async (req: Request, res: Response) => {
+    try {
+        const getAllUsers = await prisma.user.findMany({
+            select: {
+                tamuId: true,
+                namaTamu: true,
+                emailTamu: true,
+                roleTamu: true,
+                statusTamu: true,
+                pekerjaan: true,
+                dibuatTanggal: true,
+                diupdateTanggal: true,
+            }
+        });
+        if (!getAllUsers) {
+            return res.json({
+                status: 404,
+                message: "Data user tidak ditemukan"
+            })
         }
-    });
+        res.json({
+            status: 200,
+            message: "Data user ditemukan",
+            datas: getAllUsers
+        })
+    } catch (error: any) {
+        return res.json({
+            status: 404,
+            message: error.message
+        })
+    }
 }
 
-const createUser = (req: Request, res: Response) => {
-    const { Nama_tamu, Email_tamu, Jenis_kelamin, Umur_tamu, Nomer_telephone_tamu, Alamat_tamu, Password, Role_tamu, Status_tamu, Pekerjaan, Dibuat_tanggal, Diupdate_tanggal } = req.body;
 
-    const salt = bcryptjs.genSaltSync(10);
-    const hashPassword = bcryptjs.hashSync(Password, salt);
-
-    db.query("INSERT INTO tamu (Nama_tamu, Email_tamu, Jenis_kelamin, Umur_tamu, Nomer_telephone_tamu, Alamat_tamu, Password, Role_tamu, Status_tamu, Pekerjaan, Dibuat_tanggal, Diupdate_tanggal) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [Nama_tamu, Email_tamu, Jenis_kelamin, Umur_tamu, Nomer_telephone_tamu, Alamat_tamu, Password, Role_tamu, Status_tamu, Pekerjaan, Dibuat_tanggal, Diupdate_tanggal], (err, result: ResultSetHeader) => {
-        if (err) {
-            respone(400, err, "error", res);
-        } else {
-            respone(200, result.affectedRows, "success", res, false);
-        }
-    })
-}
-
-
-export { getUsers, createUser };
+export { getUsers };
