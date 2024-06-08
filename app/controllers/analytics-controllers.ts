f67f606d649b5a7c40dcf9b1cbc507bfedde2624
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-06-08 23:11:58 +0700

    add: totalRevneue

--- app/controllers/analytics-controllers.ts
+++ app/controllers/analytics-controllers.ts
@@ -4,6 +4,103 @@ import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
+export const getTotalRevenue = async (req: Request, res: Response) => {
+  const { period } = req.query; // 'daily', 'bulanan', 'tahunan'
+  let dateFilter;
+
+  switch (period) {
+    case 'daily':
+      dateFilter = startOfDay(new Date());
+      break;
+    case 'bulanan':
+      dateFilter = startOfMonth(new Date());
+      break;
+    case 'tahunan':
+      dateFilter = startOfYear(new Date());
+      break;
+    default:
+      dateFilter = startOfMonth(new Date()); // Default ke bulanan jika tidak spesifik
+      break;
+  }
+
+  try {
+    const totalRevenue = await prisma.pembayaran.aggregate({
+      _sum: {
+        jumlahBayar: true
+      },
+      where: {
+        statusPembayaran: "lunas",
+        tanggalBayar: {
+          gte: dateFilter
+        }
+      }
+    });
+
+    res.json({
+      status: 200,
+      message: `Total pendapatan dari periode ${period} berhasil diambil.`,
+      data: {
+        totalPendapatan: totalRevenue._sum.jumlahBayar || 0 // Jika tidak ada pembayaran, kembalikan 0
+      }
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      status: 400,
+      message: error.message
+    });
+  }
+};
+
+export const getMonthlyRevenue = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const currentYear = new Date().getFullYear();
+    const startOfYearDate = startOfYear(new Date());
+
+    // Inisialisasi pendapatan per bulan dengan total 0
+    const monthlyRevenue: { [key: string]: { total: number } } = {};
+    for (let month = 0; month < 12; month++) {
+      monthlyRevenue[month] = { total: 0 };
+    }
+
+    const payments = await prisma.pembayaran.findMany({
+      where: {
+        tanggalBayar: {
+          gte: startOfYearDate,
+        },
+        statusPembayaran: "lunas",
+        reservasi: {
+          statusReservasi: "Diterima"
+        }
+      },
+      select: {
+        tanggalBayar: true,
+        jumlahBayar: true
+      }
+    });
+
+    payments.forEach(payment => {
+      const month = payment.tanggalBayar.getMonth();
+      monthlyRevenue[month].total += payment.jumlahBayar;
+    });
+
+    const formattedData = Object.keys(monthlyRevenue).map(month => ({
+      name: format(new Date(currentYear, parseInt(month)), 'MMM'),
+      total: monthlyRevenue[month].total
+    }));
+
+    res.json({
+      status: 200,
+      message: `Pendapatan bulanan untuk tahun ${currentYear} berhasil diambil.`,
+      data: formattedData,
+      year: currentYear
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      status: 400,
+      message: error.message,
+    });
+  }
+};
 
 export const getAnalytics = async (req: Request, res: Response) => {
   const { period } = req.query; // 'daily', 'bulanan', 'tahunan'
@@ -101,54 +198,3 @@ export const getAnalytics = async (req: Request, res: Response) => {
     });
   }
 }
-
-export const getMonthlyRevenue = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const currentYear = new Date().getFullYear();
-    const startOfYearDate = startOfYear(new Date());
-
-    // Inisialisasi pendapatan per bulan dengan total 0
-    const monthlyRevenue: { [key: string]: { total: number } } = {};
-    for (let month = 0; month < 12; month++) {
-      monthlyRevenue[month] = { total: 0 };
-    }
-
-    const payments = await prisma.pembayaran.findMany({
-      where: {
-        tanggalBayar: {
-          gte: startOfYearDate,
-        },
-        statusPembayaran: "lunas",
-        reservasi: {
-          statusReservasi: "Diterima"
-        }
-      },
-      select: {
-        tanggalBayar: true,
-        jumlahBayar: true
-      }
-    });
-
-    payments.forEach(payment => {
-      const month = payment.tanggalBayar.getMonth();
-      monthlyRevenue[month].total += payment.jumlahBayar;
-    });
-
-    const formattedData = Object.keys(monthlyRevenue).map(month => ({
-      name: format(new Date(currentYear, parseInt(month)), 'MMM'),
-      total: monthlyRevenue[month].total
-    }));
-
-    res.json({
-      status: 200,
-      message: `Pendapatan bulanan untuk tahun ${currentYear} berhasil diambil.`,
-      data: formattedData,
-      year: currentYear
-    });
-  } catch (error: any) {
-    res.status(400).json({
-      status: 400,
-      message: error.message,
-    });
-  }
-};
