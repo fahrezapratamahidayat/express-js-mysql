26899d257dc583a797a8b41f09f69122d972599d
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-06-16 22:52:10 +0700

    fix import

--- app/controllers/users.ts
+++ app/controllers/users.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
